74f8e70b84a3f5925e61c3d4bf4b1f3970fe556c
joyusa2002 <joyusa2002@yahoo.com>
2015-02-21 18:36:45 -0600

    Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This code was written by Chitra Shukla to test the inverse caching concept 
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+chitraMatrix <- function(x = matrix()) {
+        i <- NULL ## Creating a blank matrix to store Inverse
+        set <- function(y) {
+                x <<- y  ## Assign new matrix
+                i <<- NULL
+        }
+        get <- function() x
+        ##Inverse calculation
+        setsolve <- function(solve) i <<- solve 
+        
+        #Cache value
+        getsolve <- function() i 
+        
+        ##Store the values in a list and return the list
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
+## Caching function
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getsolve() #Check if matrix x is cached
+        if(!is.null(i)) {
+                message("getting cached data") # get the cached value
+                return(i)
+        }
+        data <- x$get() #Calculating inverse
+        i <- solve(data, ...) 
+        x$setsolve(i) #Caching Inverse data
+        i
 }
+
